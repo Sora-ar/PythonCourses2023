bc0c3ddc39b3bea6d8f1ad1166f84afe95e5e6ce
Alexandra <atamanenkoa@school73kharkov.com>
2023-07-26 14:46:21 +0300

    ready number 19 of lab 1.

---
+++ lab1_v19.py
@@ -0,0 +1,62 @@
+import random
+
+N, M = (5, 5)
+arr = []
+
+
+def sumMinItems():
+    print('')
+    print('part 2')
+    min_sum = arr[0][-1]
+    print('Start min sum = ', min_sum)
+    print('')
+    for i in range(1, N):
+        l_side = 0
+        r_side = 0
+        for j in range(M - i):
+            l_side += arr[i + j][j]
+            r_side += arr[j][i + j]
+        if l_side < min_sum: min_sum = l_side
+        if r_side < min_sum: min_sum = r_side
+        print('Left side ', l_side)
+        print('Right side ', r_side)
+    print('')
+    print('Min sum: ', min_sum)
+
+
+def sumNonNegativRows():
+    print('part 1')
+    for i in range(N):
+        counter = 0
+        stop = True
+        for j in range(M):
+            counter += arr[i][j]
+            if arr[i][j] < 0:
+                stop = False
+                break
+        if stop:
+            print('Row ', i + 1, ': sum = ', counter)
+        else:
+            print(i + 1, ' row: there is a negative element')
+
+
+def printArray(lst):
+    for i in range(len(lst)):
+        for j in range(len(lst[i])):
+            print(f"{lst[i][j]} ", end=' ')
+        print()
+
+    print('')
+
+
+def createArray():
+    for i in range(N):
+        arr.append([])
+        for j in range(M):
+            arr[i].append(random.randint(-9, 9))
+
+
+createArray()
+printArray(arr)
+sumNonNegativRows()
+sumMinItems()
